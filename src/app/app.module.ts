5355ea698b5b0db07ac6bc2f7fd4c2ffbd1ebd3b
KevinKlinkhammer <kevin.klinkhammer@stud.hslu.ch>
2018-05-29 13:25:34 +0200

    initial commit travis firebase pipeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9UYMPDqVqA8S_LJR3eWo2C7NQVas0mnw",
+  authDomain: "ionic-template-pipeline.firebaseapp.com",
+  databaseURL: "https://ionic-template-pipeline.firebaseio.com",
+  projectId: "ionic-template-pipeline",
+  storageBucket: "ionic-template-pipeline.appspot.com",
+  messagingSenderId: "582009042595"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
